5b71016daa895cf401aa93e12d7503cb89bbe0db
Fabian Zimmermann <fabian@blindpage.de>
2026-06-05 12:31:05 +0200

    fix: Advancing ToDos ueber den Akkordeons

--- app/modules/termine/AdvancingView.tsx
+++ app/modules/termine/AdvancingView.tsx
@@ -12,12 +12,8 @@ interface AdvancingViewProps {
 export default function AdvancingView({ terminId, isAdmin }: AdvancingViewProps) {
   return (
     <div className="flex flex-col gap-4" style={{ maxWidth: '1200px' }}>
-      <div className="grid grid-cols-3 gap-4 items-start">
-        <div className="col-span-2">
-          <AdvancingCard terminId={terminId} isAdmin={isAdmin} />
-        </div>
-        <ToDoCard terminId={terminId} />
-      </div>
+      <ToDoCard terminId={terminId} />
+      <AdvancingCard terminId={terminId} isAdmin={isAdmin} />
       <TerminFileCard terminId={String(terminId)} />
     </div>
   )
