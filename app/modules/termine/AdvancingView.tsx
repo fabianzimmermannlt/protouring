b42fa9b221330c2193ad80931a84367049296bed
Fabian Zimmermann <fabian@blindpage.de>
2026-06-05 09:58:15 +0200

    feat: ToDos im Advancing-View

--- app/modules/termine/AdvancingView.tsx
+++ app/modules/termine/AdvancingView.tsx
@@ -2,6 +2,7 @@
 
 import AdvancingCard from './AdvancingCard'
 import TerminFileCard from './TerminFileCard'
+import ToDoCard from './ToDoCard'
 
 interface AdvancingViewProps {
   terminId: number
@@ -11,6 +12,7 @@ interface AdvancingViewProps {
 export default function AdvancingView({ terminId, isAdmin }: AdvancingViewProps) {
   return (
     <div className="flex flex-col gap-4" style={{ maxWidth: '1200px' }}>
+      <ToDoCard terminId={terminId} />
       <AdvancingCard terminId={terminId} isAdmin={isAdmin} />
       <TerminFileCard terminId={String(terminId)} />
     </div>
